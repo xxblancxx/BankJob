f2c04f9e8dfb939071086973ca97b54e9447dcff
xxblancxx <xxblancxx@hotmail.com>
2016-12-20 21:14:12 +0100

    Added test for testing connection to broker

--- LoanBrokerTests/LoanBrokerTests.cs
+++ LoanBrokerTests/LoanBrokerTests.cs
@@ -4,7 +4,7 @@ using LoanBroker.RuleBase;
 using System.Text;
 using System.Collections.Generic;
 using System.Linq;
-
+using RabbitMQ.Client;
 
 namespace LoanBrokerTests
 {
@@ -18,15 +18,15 @@ namespace LoanBrokerTests
             var banks = new List<RuleBase.Bank>();
             banks.Add(new RuleBase.Bank("CphXMLBank", 500, 999999999, new DateTime(3000, 01, 01), false, "datdb.cphbusiness.dk", "cphbusiness.bankXML"));
             banks.Add(new RuleBase.Bank("CphJSONBank", 500, 999999999, new DateTime(3000, 01, 01), false, "datdb.cphbusiness.dk", "cphbusiness.bankJSON"));
-           
+
             // Test loanrequest, as we don't know if banks will respond to all kind of data sent.
             var request = new LoanBroker.LoanRequest("0101011122", 400, 10000, new DateTime(1970, 05, 05));
 
-           
+
 
             foreach (var bank in banks)
             {
-                string queueForBank = "HUNDIGETESTQUEUE"+bank.Name;
+                string queueForBank = "HUNDIGETESTQUEUE" + bank.Name;
                 // Make sure our queue exists.
                 LoanBroker.MessageSender.DeclareQueue(bank.Host, queueForBank);
 
@@ -54,6 +54,13 @@ namespace LoanBrokerTests
             responses = responses.Where(r => r != null).ToList();
 
             Assert.AreEqual(banks.Count, responses.Count); // If number of banks equals to number of responses recieved, every bank has answered and all banks are online.
+        }
+
+        [TestMethod]
+        public void TestMessageBroker()
+        {
+            var factory = new ConnectionFactory() { HostName = "datdb.cphbusiness.dk" };
+            var connection = factory.CreateConnection();
         }
     }
 }
